0901e767af81803b8aa7021ffa30b62106c1fa6e
hexbokeh <jwallachstudent@gmail.com>
2018-04-14 23:21:48 -0400

    Testing things in Loop for now

--- finalproject.ino
+++ finalproject.ino
@@ -11,10 +11,29 @@ int rowPowerPins[4] = {3,4,5,6};
 
 void setup() 
 {
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+
+  for (int i = 0; i < 4; i++)
+  {
+    pinMode(rowPowerPins[i], OUTPUT);
+  }
 }
 
 void loop() 
 {
-  digitalWrite(ledPin, HIGH);
+
+  // debug
+  Serial.print("current power pin: ");
+  Serial.print(rowPowerPins[currentStep]);
+  Serial.print(" currentStep = ");
+  Serial.println(currentStep);
+
+  // turning on power.
+  digitalWrite(rowPowerPins[currentStep], HIGH);
+  
+  delay(stepDuration);
+
+  digitalWrite(rowPowerPins[currentStep], LOW);
+  currentStep ++;
+  currentStep %= 4;
 }
