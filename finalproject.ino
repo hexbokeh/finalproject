dc818f693df1d7d76a6138576aabb28063dc886e
hexbokeh <jwallachstudent@gmail.com>
2018-04-20 16:57:00 -0400

    finished combining LED test

--- finalproject.ino
+++ finalproject.ino
@@ -2,12 +2,10 @@
 
 
 int stepLength = 250; // default, in milliseconds.
-int ledPins[4] = {0, 1, 2, 3};
+int ledPins[4] = {0, 1, 2, 3}; // not including ground.
 int currentStep; // 0-3 (4 steps)
 int potMatrix[4][5];
-
-int stepDuration = 100;
-int rowPowerPins[4] = {9, 10, 11, 12};
+int rowPowerPins[4] = {8, 9, 10, 11}; // not including ground
 
 
 void setup()
@@ -28,18 +26,28 @@ void setup()
 void loop()
 {
 
+  // six values: step #, pot values 1-5.
+  // variables: a,b,c,d,e,f.
+  // a should correspond to the step (in this case, the row of the array)
+
   // debug
   Serial.print("current power pin: ");
   Serial.print(rowPowerPins[currentStep]);
   Serial.print(" currentStep = ");
   Serial.println(currentStep);
 
+
+
+
   // turning on power.
   digitalWrite(rowPowerPins[currentStep], HIGH);
+  digitalWrite(ledPins[currentStep], HIGH);
 
-  delay(stepDuration);
+  delay(stepLength);
 
+  // turning off power.
   digitalWrite(rowPowerPins[currentStep], LOW);
+  digitalWrite(ledPins[currentStep], LOW);
   currentStep ++;
   currentStep %= 4;
 }
