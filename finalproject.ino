f43cfba6dd8f1cf14840518bec87afc965ed5c80
hexbokeh <jwallachstudent@gmail.com>
2018-04-20 16:51:07 -0400

    combining serial test files

--- finalproject.ino
+++ finalproject.ino
@@ -1,17 +1,23 @@
 #include <MIDI.h>
 
 
-int ledPin = 13;
+int stepLength = 250; // default, in milliseconds.
+int ledPins[4] = {0, 1, 2, 3};
 int currentStep; // 0-3 (4 steps)
 int potMatrix[4][5];
 
 int stepDuration = 100;
-int rowPowerPins[4] = {3,4,5,6};
+int rowPowerPins[4] = {9, 10, 11, 12};
 
 
-void setup() 
+void setup()
 {
   Serial.begin(9600);
+
+  for (int i = 0; i < 4; i++)
+  {
+    pinMode(ledPins[i], OUTPUT);
+  }
 
   for (int i = 0; i < 4; i++)
   {
@@ -19,7 +25,7 @@ void setup()
   }
 }
 
-void loop() 
+void loop()
 {
 
   // debug
@@ -30,7 +36,7 @@ void loop()
 
   // turning on power.
   digitalWrite(rowPowerPins[currentStep], HIGH);
-  
+
   delay(stepDuration);
 
   digitalWrite(rowPowerPins[currentStep], LOW);
