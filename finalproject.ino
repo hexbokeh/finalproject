2fb27ed8ef179288179bae632fa36386e0a246fd
hexbokeh <jwallachstudent@gmail.com>
2018-04-20 17:13:30 -0400

    added pot matrix pins, added array to loop()
    Keep the pins where they are!
    The matrix could eventually be broken out into its own function.

--- finalproject.ino
+++ finalproject.ino
@@ -1,11 +1,25 @@
 #include <MIDI.h>
-
+// Serial included by default, as well.
 
 int stepLength = 250; // default, in milliseconds.
 int ledPins[4] = {0, 1, 2, 3}; // not including ground.
 int currentStep; // 0-3 (4 steps)
+
+int potMatrixPins[4][5] =
+{
+  {A0, A1, A2, A3, A4},
+  {A5, A6, A7, A8, A9},
+  {A13, A14, A15, A16, A17},
+  {A18, A19, A20, A21, A22},
+}
+
 int potMatrix[4][5];
+
 int rowPowerPins[4] = {8, 9, 10, 11}; // not including ground
+
+
+// specific analog reading row pins
+
 
 
 void setup()
@@ -42,6 +56,15 @@ void loop()
   // turning on power.
   digitalWrite(rowPowerPins[currentStep], HIGH);
   digitalWrite(ledPins[currentStep], HIGH);
+
+  //read pot values for the row.
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 5; j++)
+    {
+      potMatrix[i][j] = analogRead(potMatrixPins[i][j]);
+    }
+  }
 
   delay(stepLength);
 
