81ce8e25723512388c0145da94bdcde2c9322d88
hexbokeh <jwallachstudent@gmail.com>
2018-04-23 01:27:26 -0400

    Sending Serial Correctly (from what I can see)
    Processing is working, too, from what I can see! Now I have to figure out how to incoprorate it into this gitHub as well.

--- finalproject.ino
+++ finalproject.ino
@@ -57,23 +57,30 @@ void loop()
   for (int j = 0; j < 5; j++)
   {
     potMatrix[currentStep][j] = 1023 - analogRead(potMatrixPins[currentStep][j]);
+    potMatrix[currentStep][j] = map(potMatrix[currentStep][j], 0, 1023, 0, 255);
   }
 
   // debug
-  if (currentStep == 2)
-  {
-    for (int j = 0; j < 5; j++)
-    {
-      Serial.print("pot # ");
-      Serial.print(j);
-      Serial.print(" = ");
-      Serial.print(potMatrix[currentStep][j]);
-      Serial.print(" ");
-    }
-  }
+  //  if (currentStep == 2)
+  //  {
+  //    for (int j = 0; j < 5; j++)
+  //    {
+  //      Serial.print("pot # ");
+  //      Serial.print(j);
+  //      Serial.print(" = ");
+  //      Serial.print(potMatrix[currentStep][j]);
+  //      Serial.print(" ");
+  //    }
+  //  }
+  //
+  //  //  Serial.println(potMatrix[currentStep][4]);
+  //  Serial.println();
 
-  //  Serial.println(potMatrix[currentStep][4]);
-  Serial.println();
+  // Serial send!!!
+  // currentStep and pot values transmitted all at once.
+
+
+  transmitSerial(); //******** Where the magic happens.
 
   // debug
   //  if((currentStep == 3 || currentStep == 4) && digitalRead(rowPowerPins[currentStep]))
@@ -81,6 +88,7 @@ void loop()
   //    Serial.println("It's working!");
   //  }
 
+  stepLength = map(potMatrix[currentStep][4], 20, 255, 10, 750); // 20 seems to be as low as they go.
   delay(stepLength);
 
   // turning off power.
@@ -91,3 +99,17 @@ void loop()
   // debug
   // currentStep = 0;
 }
+
+// six values: step #, pot values 1-5.
+// variables: a,b,c,d,e,f.
+// a should correspond to the step (in this case, the row of the array)
+void transmitSerial()
+{
+  Serial.write(currentStep);
+  for (int i = 0; i < 5; i++) // it's 5, not 6!!!
+  {
+    Serial.write(potMatrix[currentStep][i]);
+  }
+
+}
+
